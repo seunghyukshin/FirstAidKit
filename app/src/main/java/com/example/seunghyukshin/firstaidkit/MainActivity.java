15d92fe5332174f26b3f518b0fc6dcfd0ad0650a
seunghyuk <sunghyuk9@naver.com>
2018-07-31 18:46:24 +0900

    add 2 Activity&Button

--- app/src/main/java/com/example/seunghyukshin/firstaidkit/MainActivity.java
+++ app/src/main/java/com/example/seunghyukshin/firstaidkit/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.seunghyukshin.firstaidkit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button_diag;
+    Button button_fa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_diag = (Button) findViewById(R.id.button_diagnosis);
+        button_fa = (Button) findViewById(R.id.button_first_aid);
+
+        button_diag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DiagnosisActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button_fa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FirstAidActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
