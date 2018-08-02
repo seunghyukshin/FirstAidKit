92c630270c8d7194a2de2d48e14663e3e01c3d97
seunghyuk <sunghyuk9@naver.com>
2018-08-02 17:59:04 +0900

    add FirstAidItem & View skeleton

---
+++ app/src/main/java/com/example/seunghyukshin/firstaidkit/FirstAidItem.java
@@ -0,0 +1,31 @@
+package com.example.seunghyukshin.firstaidkit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstAidItem {
+
+    String name;
+    String contents;
+
+    public FirstAidItem(String name,String contents){
+        this.name= name;
+        this.contents= contents;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ app/src/main/java/com/example/seunghyukshin/firstaidkit/FirstAidItemView.java
@@ -0,0 +1,34 @@
+package com.example.seunghyukshin.firstaidkit;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by SEUNGHYUK SHIN on 2018-08-02.
+ */
+
+public class FirstAidItemView extends LinearLayout{
+    TextView textView;
+    TextView textView2;
+    public FirstAidItemView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public void init(Context context){
+        LayoutInflater inflater =  (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.first_aid_item,this,true);
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView2 = (TextView) findViewById(R.id.textView2);
+    }
+
+    public void setName(String name){
+        textView.setText(name);
+    }
+    public void setContent(String content){
+        textView2.setText(content);
+    }
+}
