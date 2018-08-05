93dca430fefdbe5dfb64fa3ec6ef56ea4c46a122
seunghyuk <sunghyuk9@naver.com>
2018-08-05 22:58:33 +0900

    add Today First Aid layout

--- app/src/main/java/com/example/seunghyukshin/firstaidkit/FirstAidActivity.java
+++ app/src/main/java/com/example/seunghyukshin/firstaidkit/FirstAidActivity.java
@@ -10,20 +10,52 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class FirstAidActivity extends AppCompatActivity {
-    ListView listView;
-    FirstAidAdapter adapter;
+    ListView listView_today_fa; //오늘의 응급상황
+    ListView listView_fa;//응급처치법 쭈루룩
+    FirstAidAdapter adapter_fa;
+    FirstAidAdapter adapter_today_fa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_aid);
-        listView = (ListView) findViewById(R.id.list_view);
 
-        adapter = new FirstAidAdapter();
-        adapter.addItem(new FirstAidItem("열사병","물을마셔라"));
-        adapter.addItem(new FirstAidItem("탈진","병원"));
-        adapter.addItem(new FirstAidItem("낙뢰","찌릿"));
+        listView_today_fa = (ListView) findViewById(R.id.listView);
+        adapter_today_fa = new FirstAidAdapter();
+        // 우선순위 알고리즘추가 할것
+        adapter_today_fa.addItem(new FirstAidItem("열사병"));
+        adapter_today_fa.addItem(new FirstAidItem("식중독"));
 
-        listView.setAdapter(adapter);
+        listView_today_fa.setAdapter(adapter_today_fa);
+
+
+
+
+
+        listView_fa = (ListView) findViewById(R.id.listView2);
+        //가나다 순으로
+        adapter_fa = new FirstAidAdapter();
+        adapter_fa.addItem(new FirstAidItem("골절"));
+        adapter_fa.addItem(new FirstAidItem("기도폐쇄"));
+        adapter_fa.addItem(new FirstAidItem("뇌수막염"));
+        adapter_fa.addItem(new FirstAidItem("뇌졸중"));
+        adapter_fa.addItem(new FirstAidItem("당뇨 응급상황"));
+        adapter_fa.addItem(new FirstAidItem("머리손상"));
+        adapter_fa.addItem(new FirstAidItem("무의식"));
+        adapter_fa.addItem(new FirstAidItem("발작/간질"));
+        adapter_fa.addItem(new FirstAidItem("심리적 응급처치"));
+        adapter_fa.addItem(new FirstAidItem("심장발작"));
+        adapter_fa.addItem(new FirstAidItem("쏘임/물림"));
+        adapter_fa.addItem(new FirstAidItem("알레르기/아나필락시스"));
+        adapter_fa.addItem(new FirstAidItem("열사병"));
+        adapter_fa.addItem(new FirstAidItem("저체온증"));
+        adapter_fa.addItem(new FirstAidItem("정신적 고통"));
+        adapter_fa.addItem(new FirstAidItem("좌상/염좌"));
+        adapter_fa.addItem(new FirstAidItem("중독/해로운 물질"));
+        adapter_fa.addItem(new FirstAidItem("천식발작"));
+        adapter_fa.addItem(new FirstAidItem("출혈"));
+        adapter_fa.addItem(new FirstAidItem("화상"));
+
+        listView_fa.setAdapter(adapter_fa);
 
 
 
@@ -34,10 +66,12 @@ public class FirstAidActivity extends AppCompatActivity {
 
 
 
+    //오늘의 응급상황
 
 
 
 
+    //응급처치법
     class FirstAidAdapter extends BaseAdapter{
         ArrayList<FirstAidItem> items = new ArrayList<FirstAidItem>();
 
@@ -66,7 +100,7 @@ public class FirstAidActivity extends AppCompatActivity {
 
             FirstAidItem item = items.get(position);
             view.setName(item.getName());
-            view.setContent(item.getContents());
+            //view.setContent(item.getContents());
             return view;
         }
     }
--- app/src/main/java/com/example/seunghyukshin/firstaidkit/FirstAidItem.java
+++ app/src/main/java/com/example/seunghyukshin/firstaidkit/FirstAidItem.java
@@ -6,26 +6,28 @@ import android.os.Bundle;
 public class FirstAidItem {
 
     String name;
-    String contents;
-
+    //String contents;
+    public FirstAidItem(String name){
+        this.name = name;
+    }
+    /*
     public FirstAidItem(String name,String contents){
         this.name= name;
         this.contents= contents;
-    }
+    }*/
 
     public void setName(String name) {
         this.name = name;
     }
-
-    public void setContents(String contents) {
-        this.contents = contents;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
     public String getName() {
         return name;
     }
+    /*
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+    public String getContents() {
+        return contents;
+    }
+*/
 }
