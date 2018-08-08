4d1efbe4a3ef031f5b130a87a4f935dc7cd03cfe
khJ <rbsgnl123@gmail.com>
2018-08-08 20:35:01 +0900

    add Diagnosis complete state

---
+++ app/src/main/java/com/example/seunghyukshin/firstaidkit/DiagnosisState.java
@@ -0,0 +1,13 @@
+package com.example.seunghyukshin.firstaidkit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DiagnosisState extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.state_diagnosis);
+    }
+}
