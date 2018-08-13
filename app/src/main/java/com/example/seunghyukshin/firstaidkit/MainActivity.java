04555d124ebec091be700f213e21f0c7eeb1940a
MaengMaeng <maybe8189@naver.com>
2018-08-13 17:57:45 +0900

    기상청 api 적용

--- app/src/main/java/com/example/seunghyukshin/firstaidkit/MainActivity.java
+++ app/src/main/java/com/example/seunghyukshin/firstaidkit/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.seunghyukshin.firstaidkit;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class MainActivity extends AppCompatActivity {
     Button button_diag;
     Button button_fa;
+
+    TextView textView_shortWeather;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +61,116 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        textView_shortWeather = (TextView)findViewById(R.id.shortWeather);
 
+        new ReceiveShortWeather().execute();
+    }
+    public class ReceiveShortWeather extends AsyncTask<URL, Integer, Long> {
 
+        ArrayList<ShortWeather> shortWeathers = new ArrayList<ShortWeather>();
+
+        protected Long doInBackground(URL... urls) {
+
+            String url = "http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1159068000";
+
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+
+            Response response = null;
+
+            try {
+                response = client.newCall(request).execute();
+                parseXML(response.body().string());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(Long result) {
+            String data = "";
+
+            for(int i=0; i<shortWeathers.size(); i++) {
+                data += shortWeathers.get(i).getDay() + "일 " +
+                        shortWeathers.get(i).getHour() + "시 " +
+                        shortWeathers.get(i).getTemp() + "도 " +
+                        shortWeathers.get(i).getWfKor() + " " +
+                        shortWeathers.get(i).getPop() + "\n";
+            }
+
+            textView_shortWeather.setText(data);
+        }
+
+        void parseXML(String xml) {
+            try {
+                String tagName = "";
+                boolean onHour = false;
+                boolean onDay = false;
+                boolean onTem = false;
+                boolean onWfKor = false;
+                boolean onPop = false;
+                boolean onEnd = false;
+                boolean isItemTag1 = false;
+                int i = 0;
+
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                XmlPullParser parser = factory.newPullParser();
+
+                parser.setInput(new StringReader(xml));
+
+                int eventType = parser.getEventType();
+
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    if (eventType == XmlPullParser.START_TAG) {
+                        tagName = parser.getName();
+                        if (tagName.equals("data")) {
+                            shortWeathers.add(new ShortWeather());
+                            onEnd = false;
+                            isItemTag1 = true;
+                        }
+                    } else if (eventType == XmlPullParser.TEXT && isItemTag1) {
+                        if (tagName.equals("hour") && !onHour) {
+                            shortWeathers.get(i).setHour(parser.getText());
+                            onHour = true;
+                        }
+                        if (tagName.equals("day") && !onDay) {
+                            shortWeathers.get(i).setDay(parser.getText());
+                            onDay = true;
+                        }
+                        if (tagName.equals("temp") && !onTem) {
+                            shortWeathers.get(i).setTemp(parser.getText());
+                            onTem = true;
+                        }
+                        if (tagName.equals("wfKor") && !onWfKor) {
+                            shortWeathers.get(i).setWfKor(parser.getText());
+                            onWfKor = true;
+                        }
+                        if (tagName.equals("pop") && !onPop) {
+                            shortWeathers.get(i).setPop(parser.getText());
+                            onPop = true;
+                        }
+                    } else if (eventType == XmlPullParser.END_TAG) {
+                        if (tagName.equals("s06") && onEnd == false) {
+                            i++;
+                            onHour = false;
+                            onDay = false;
+                            onTem = false;
+                            onWfKor = false;
+                            onPop = false;
+                            isItemTag1 = false;
+                            onEnd = true;
+                        }
+                    }
+
+                    eventType = parser.next();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
