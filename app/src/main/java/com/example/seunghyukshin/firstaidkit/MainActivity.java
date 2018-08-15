d20a2f97a877a1e0a85ed543fca57a7e9f458596
MaengMaeng <maybe8189@naver.com>
2018-08-15 15:26:08 +0900

    푸시 알림 테스트

--- app/src/main/java/com/example/seunghyukshin/firstaidkit/MainActivity.java
+++ app/src/main/java/com/example/seunghyukshin/firstaidkit/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.seunghyukshin.firstaidkit;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
@@ -80,7 +83,25 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
         //or switch문을 이용하면 될듯 하다.
         if (id == android.R.id.home) {
+
+            NotificationManager notificationManager= (NotificationManager)MainActivity.this.getSystemService(MainActivity.this.NOTIFICATION_SERVICE);
+            Intent intent1 = new Intent(MainActivity.this.getApplicationContext(),MainActivity.class); //인텐트 생성.
+
+            Notification.Builder builder = new Notification.Builder(getApplicationContext());
+            intent1.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP| Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            //현재 액티비티를 최상으로 올리고, 최상의 액티비티를 제외한 모든 액티비티를 없앤다.
+
+            PendingIntent pendingNotificationIntent = PendingIntent.getActivity( MainActivity.this,0, intent1,PendingIntent.FLAG_UPDATE_CURRENT);
+            //PendingIntent는 일회용 인텐트 같은 개념입니다.
+            builder.setSmallIcon(R.drawable.setting_icon).setTicker("HETT").setWhen(System.currentTimeMillis())
+                    .setNumber(1).setContentTitle("푸쉬 제목").setContentText("푸쉬내용")
+                    .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE).setContentIntent(pendingNotificationIntent).setAutoCancel(true).setOngoing(true);
+            //해당 부분은 API 4.1버전부터 작동합니다.
+
+            notificationManager.notify(1, builder.getNotification()); // Notification send
+
             Toast.makeText(this, "홈", Toast.LENGTH_SHORT).show();
+
             return true;
         }
         if (id == R.id.action_setting) {
