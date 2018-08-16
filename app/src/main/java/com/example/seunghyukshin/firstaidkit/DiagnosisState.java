fa5a0aef34e5c1e3cec0a93a5577cbc704bf8b3b
khJ <rbsgnl123@gmail.com>
2018-08-16 19:45:59 +0900

    add DiagState symptom

--- app/src/main/java/com/example/seunghyukshin/firstaidkit/DiagnosisState.java
+++ app/src/main/java/com/example/seunghyukshin/firstaidkit/DiagnosisState.java
@@ -3,12 +3,15 @@ package com.example.seunghyukshin.firstaidkit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class DiagnosisState extends AppCompatActivity{
+public class DiagnosisState extends AppCompatActivity {
+
+    TextView textView_symptom;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.state_diagnosis);
 
@@ -16,6 +19,19 @@ public class DiagnosisState extends AppCompatActivity{
 
         int[] list = intent.getIntArrayExtra("list");
 
-        Toast.makeText(getApplicationContext(), list[0] + ", " + list[1] + ", " + list[2], Toast.LENGTH_SHORT ).show();
+        Toast.makeText(getApplicationContext(), list[0] + ", " + list[1] + ", " + list[2], Toast.LENGTH_SHORT).show();
+
+        textView_symptom = (TextView) findViewById(R.id.symptom);
+
+        String symptom="";
+        for (int i = 0; i < list.length; i++) {
+            if (list[0] == 1) {
+                symptom = "머리손상";
+            }
+            if (list[1] == 1 || list[2] == 1 || list[3] == 1 || list[4] == 1) {
+                symptom = "열사병";
+            }
+        }
+        textView_symptom.setText(symptom);
     }
 }
