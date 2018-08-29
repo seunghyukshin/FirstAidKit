8707fb7e0429003de1c462350e9e2a4f106c9a41
seunghyuk <sunghyuk9@naver.com>
2018-08-29 21:10:24 +0900

    revise to onClickListener on Widget

--- app/src/main/java/com/example/seunghyukshin/firstaidkit/NewAppWidget.java
+++ app/src/main/java/com/example/seunghyukshin/firstaidkit/NewAppWidget.java
@@ -13,24 +13,23 @@ import android.widget.TextView;
  * Implementation of App Widget functionality.
  */
 public class NewAppWidget extends AppWidgetProvider {
-    static Context mContext;
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-        /*
-        CharSequence widgetText =
-                ((MainActivity) mContext).helper.getText().toString();
-        */
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-        SharedPreferences sharedPreferences = context.getSharedPreferences("com.example.seunghyukshin.firstaidkit.sharedPreferences",Context.MODE_PRIVATE);
-        CharSequence widgetText = sharedPreferences.getString("widget_text","default");
-        views.setImageViewResource(R.id.appwidget_image,sharedPreferences.getInt("widget_image",R.drawable.firstaid));
-        views.setTextViewText(R.id.appwidget_text, widgetText);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
+//    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+//                                int appWidgetId) {
+//        /*
+//        CharSequence widgetText =
+//                ((MainActivity) mContext).helper.getText().toString();
+//        */
+//        // Construct the RemoteViews object
+//        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+//        SharedPreferences sharedPreferences = context.getSharedPreferences("com.example.seunghyukshin.firstaidkit.sharedPreferences",Context.MODE_PRIVATE);
+//        CharSequence widgetText = sharedPreferences.getString("widget_text","default");
+//        views.setImageViewResource(R.id.appwidget_image,sharedPreferences.getInt("widget_image",R.drawable.firstaid));
+//        views.setTextViewText(R.id.appwidget_text, widgetText);
+//
+//        // Instruct the widget manager to update the widget
+//        appWidgetManager.updateAppWidget(appWidgetId, views);
+//    }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {// There may be multiple widgets active, so update all of them
@@ -47,9 +46,32 @@ public class NewAppWidget extends AppWidgetProvider {
             PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
             remoteViews.setOnClickPendingIntent(R.id.widget_layout,pendingIntent);
 */
+
+
             //
             remoteViews.setImageViewResource(R.id.appwidget_image,sharedPreferences.getInt("widget_image",R.drawable.firstaid));
-            remoteViews.setTextViewText(R.id.appwidget_text,sharedPreferences.getString("widget_text","haha"));
+
+            String widgetText=sharedPreferences.getString("widget_text","haha");
+            remoteViews.setTextViewText(R.id.appwidget_text,widgetText);
+
+
+            // Construct the RemoteViews object
+            // views.setTextViewText(R.id.appwidget_text, widgetText);
+            Intent intent;
+            if(widgetText.equals("저체온증")){
+                intent=new Intent(context, FirstAidDetailListActivity.class);
+                intent.putExtra("NAME",widgetText);
+            }else{
+                intent=new Intent(context, FirstAidListActivity.class);
+                intent.putExtra("NAME",widgetText);
+            }
+            PendingIntent pe=PendingIntent.getActivity(context, 0, intent, 0);
+            remoteViews.setOnClickPendingIntent(R.id.widget_layout, pe);
+            // Instruct the widget manager to update the widget
+            //appWidgetManager.updateAppWidget(appWidgetId, views);
+
+
+
             appWidgetManager.updateAppWidget(appWidgetId,remoteViews);
            // updateAppWidget(context, appWidgetManager, appWidgetId);
         }
